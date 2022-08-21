711e161f56d474a1b4fd9e4a27f541d9ba785b88
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2022-08-21 18:48:20 +0300

    Добавил параллелизм с помощью модуля node:cluster

--- sources/index.js
+++ sources/index.js
@@ -3,7 +3,10 @@
 import "./environment_initialization.js";
 import {ProxyServer} from "./proxyServer.js";
 import {Logger} from "./logger.js";
+import {cpus} from "node:os";
+import cluster from "node:cluster";
 import config from "config";
+
 
 const hostAddress = config.get("ProxyServer.hostAddress");
 const listeningPort = config.get("ProxyServer.listeningPort");
@@ -12,13 +15,21 @@ const loggingEnabled = config.get("Logger.loggingEnabled");
 const logsDirectory = config.get("Logger.logsDirectory");
 const logFileName = config.get("Logger.logFileName");
 
-let proxyServer;
-if (loggingEnabled) {
-    let logger = new Logger(`${logsDirectory}/${logFileName}`);
-    proxyServer = new ProxyServer(logger);
-}
-else {
-    proxyServer = new ProxyServer();
+const LOGICAL_CPU_CORE_QUANTITY = cpus().length;
+if (cluster.isPrimary) {
+    for (let process_number = 0; process_number < LOGICAL_CPU_CORE_QUANTITY; process_number++) {
+        cluster.fork();
+    }
+} else {
+    let proxyServer;
+    if (loggingEnabled) {
+        let logger = new Logger(`${logsDirectory}/${logFileName}`);
+        proxyServer = new ProxyServer(logger);
+    }
+    else {
+        proxyServer = new ProxyServer();
+    }
+
+    proxyServer.start(hostAddress, listeningPort);
 }
 
-proxyServer.start(hostAddress, listeningPort);
